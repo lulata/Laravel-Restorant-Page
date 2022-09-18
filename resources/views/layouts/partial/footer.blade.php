6ae71cb46d63007b7992f6ade97345aabbf82b0b
David Atanasoski <davidatanasoski@hotmail.com>
2022-09-18 14:49:42 +0200

    init

---
+++ resources/views/layouts/partial/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <p class="copyright pull-right">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>
+            <strong> Developed by</strong>
+                        Lulata
+        </p>
+    </div>
+</footer>
